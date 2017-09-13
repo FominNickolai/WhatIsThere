fc214a0ba5291ee4afd815cf2b8f67d1d8413015
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-13 23:02:38 +0300

    Added CoreLocation Manger for retrieve User Location. Added methods for check user permision and center map depending of user location.

--- WhatIsThere/Controller/MapVC.swift
+++ WhatIsThere/Controller/MapVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
@@ -15,20 +16,28 @@ class MapVC: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    //MARK: - Properties
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1_000
+    
     //MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     //MARK: - IBActions
     
     @IBAction func centerMapButtonWasPressed(_ sender: UIButton) {
         
-        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 }
@@ -36,7 +45,43 @@ class MapVC: UIViewController {
 //MARK: - MKMapViewDelegate
 extension MapVC: MKMapViewDelegate {
     
+    //MARK: - Center Map On User Location
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
+
+//MARK: - CLLocationManagerDelegate
+extension MapVC: CLLocationManagerDelegate {
+    
+    //MARK: - Configure Permision to Check if we can use location
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
