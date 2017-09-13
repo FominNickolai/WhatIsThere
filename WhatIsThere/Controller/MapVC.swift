1ac19db788e788935e8e39deff761b417055e769
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-13 22:12:31 +0300

    Some Initial Refactoring

---
+++ WhatIsThere/Controller/MapVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WhatIsThere
+//
+//  Created by Fomin Mykola on 9/13/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
