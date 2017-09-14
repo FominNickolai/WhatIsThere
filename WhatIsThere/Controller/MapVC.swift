4831a5a80bf66f808f51057fee78b28da4d07e8f
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-14 15:14:50 +0300

    Comleted to download images from flickr and save them in array.

--- WhatIsThere/Controller/MapVC.swift
+++ WhatIsThere/Controller/MapVC.swift
@@ -10,6 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 import Alamofire
+import AlamofireImage
 
 class MapVC: UIViewController {
 
@@ -32,6 +33,7 @@ class MapVC: UIViewController {
     var collectionViewFlowLayout = UICollectionViewFlowLayout()
     
     var imageUrlArray = [String]()
+    var imagesArray = [UIImage]()
     
     //MARK: - Life Cycle
 
@@ -86,6 +88,7 @@ class MapVC: UIViewController {
     
     //MARK: - Animate PopUpView Down
     @objc func animateViewDown() {
+        cancelAllSessions()
         pullUpViewHeightConstraint.constant = 0
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
@@ -129,6 +132,50 @@ class MapVC: UIViewController {
         }
     }
     
+    //MARK: - Retrieve URL For Annotation
+    func retrieveUrls(forAnnotation annotation: DroppablePin, complitionHandler: @escaping (_ status: Bool) -> ()) {
+        imageUrlArray = []
+        
+        Alamofire.request(DataService.instance.getFickerUrl(forApiKey: API_KEY, withAnnotation: annotation, andNumbersOfPhotos: 40)).responseJSON { (response) in
+            
+            guard let json = response.result.value as? Dictionary<String, AnyObject> else { return }
+            let photosDict = json["photos"] as! Dictionary<String, AnyObject>
+            let photoDictArray = photosDict["photo"] as! [Dictionary<String, AnyObject>]
+            for photo in photoDictArray {
+                let postUrl = "https://farm\(photo["farm"]!).staticflickr.com/\(photo["server"]!)/\(photo["id"]!)_\(photo["secret"]!)_h_d.jpg"
+                self.imageUrlArray.append(postUrl)
+            }
+            complitionHandler(true)
+        }
+    }
+    
+    //MARK: - Retrieve Images from Flickr
+    func retrieveImages(completionHandler: @escaping (_ status: Bool) -> ()) {
+        imagesArray = []
+        
+        for url in imageUrlArray {
+            Alamofire.request(url).responseImage(completionHandler: { (response) in
+                guard let image = response.result.value else { return }
+                self.imagesArray.append(image)
+                self.progressLbl?.text = "\(self.imagesArray.count)/40 IMAGES DOWNLOADED"
+                
+                if self.imagesArray.count == self.imageUrlArray.count {
+                    completionHandler(true)
+                }
+            })
+        }
+    }
+    
+    //MARK: Cancel All Sessions
+    func cancelAllSessions() {
+        Alamofire.SessionManager.default.session.getTasksWithCompletionHandler { (sessionDataTask, uploadData, downloadData) in
+            
+            sessionDataTask.forEach({$0.cancel()})
+            downloadData.forEach({$0.cancel()})
+            
+        }
+    }
+    
 }
 
 //MARK: - MKMapViewDelegate
@@ -155,7 +202,7 @@ extension MapVC: MKMapViewDelegate {
     
     //MARK: - Drop Pin
     @objc func dropPin(sender: UITapGestureRecognizer) {
-        
+        cancelAllSessions()
         removePin()
         removeSpinner()
         removeProgressLabel()
@@ -174,8 +221,20 @@ extension MapVC: MKMapViewDelegate {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
         
-        retrieveUrls(forAnnotation: annotation) { (succes) in
-            print(self.imageUrlArray)
+        retrieveUrls(forAnnotation: annotation) { (finished) in
+            if finished {
+                self.retrieveImages(completionHandler: { (finished) in
+                    if finished {
+                        //hide spinner
+                        self.removeSpinner()
+                        //hide label
+                        self.removeProgressLabel()
+                        // TODO: TODO reload collectionView
+                        
+                        
+                    }
+                })
+            }
         }
     }
     
@@ -188,22 +247,6 @@ extension MapVC: MKMapViewDelegate {
         
     }
     
-    //MARK: - Retrieve URL For Annotation
-    func retrieveUrls(forAnnotation annotation: DroppablePin, complitionHandler: @escaping (_ status: Bool) -> ()) {
-        imageUrlArray = []
-        
-        Alamofire.request(DataService.instance.getFickerUrl(forApiKey: API_KEY, withAnnotation: annotation, andNumbersOfPhotos: 40)).responseJSON { (response) in
-            
-            guard let json = response.result.value as? Dictionary<String, AnyObject> else { return }
-            let photosDict = json["photos"] as! Dictionary<String, AnyObject>
-            let photoDictArray = photosDict["photo"] as! [Dictionary<String, AnyObject>]
-            for photo in photoDictArray {
-                let postUrl = "https://farm\(photo["farm"]!).staticflickr.com/\(photo["server"]!)/\(photo["id"]!)_\(photo["secret"]!)_h_d.jpg"
-                self.imageUrlArray.append(postUrl)
-            }
-            complitionHandler(true)
-        }
-    }
 }
 
 //MARK: - CLLocationManagerDelegate
